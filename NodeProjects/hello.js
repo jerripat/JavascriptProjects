e733c4c829abe83568ae5282c01e4f8d48e025fc
jerripat <jerripatterson@gmail.com>
2023-04-17 11:35:00 -0500

    initial uploade for full Stack Course

--- NodeProjects/hello.js
+++ NodeProjects/hello.js
@@ -5,6 +5,10 @@ http.createServer(function (req, res) {
   fs.readFile('index.html', function (err, data) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write(data);
+    console.log('...incoming ' +  req.url);
     res.end();
   })
 }).listen(8080);
+
+
+console.log('Server running at port 8080')
